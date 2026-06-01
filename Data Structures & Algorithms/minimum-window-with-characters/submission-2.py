ebb7f590cc3bc3e6f33aefe4bbf90e651779c5e0
Yi Yang <147130934+yy31104@users.noreply.github.com>
2026-06-01 22:35:24 +0200

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        best = float("inf")
+        left = 0
+        count_t = {}
+        count_s = {}
+        ans = ""
+        have = 0
+        for i in t:
+            if i not in count_t:
+                count_t[i] = 0
+            count_t[i] += 1
+        need = len(count_t)
+        for right in range(len(s)):
+            ch = s[right]
+            if ch in count_t:
+                count_s[ch] = count_s.get(ch, 0) + 1
+                if count_t[ch] == count_s[ch]:
+                    have += 1
+
+            while need == have:
+                if best > right - left + 1:
+                        best = right - left + 1
+                        ans = s[left: right + 1]
+                if s[left] in count_t:
+                    count_s[s[left]] -= 1
+                    if count_s[s[left]] < count_t[s[left]]:
+                        have -= 1
+                left += 1
+        return ans
+
+
+                    
+
+
+            
+
+        
+
